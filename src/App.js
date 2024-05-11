8ae1f958007dd7e3f6b76409f71e2b2e8b7830b2
tgg <444202104@qq.com>
2024-05-11 16:05:15 +0800

    feat: 正确的split比例

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SplitPane from 'react-split-pane';
 function App() {
   return (
     <div className="App">
-      <SplitPane split="vertical" minSize={50}>
+      <SplitPane split="vertical" defaultSize="40%" primary="first">
         <div>123</div>
         <div>456</div>
       </SplitPane>
